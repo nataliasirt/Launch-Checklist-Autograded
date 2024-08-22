4b7ba4fcde210e2cfcc932b121f9247ab68c2776
nsirtak <nsirtak@gmail.com>
2024-08-22 18:29:38 -0500

    part 2

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Write your JavaScript code here!
-
-window.addEventListener("load", function() {
-
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
-    
- });
+// Task 2: Adding Validation
+//adds EventListener to the document object to listen for submit events.
+document.addEventListener("submit", function(event) 
+     {
+      //prevents the default form submission behavior
+       event.preventDefault(); 
+       //gets the value of the input element with the name pilotName etc
+       const pilotName = document.querySelector("input[name=pilotName]").value;
+       const copilotName = document.querySelector("input[name=copilotName]").value;
+       const fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+       const cargoMass = document.querySelector("input[name=cargoMass]").value;
+       const list = document.getElementById("faultyItems");
+       //calls the formSubmission function, passing in the document object, the list element, and the values of the input elements.
+       formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+     }
+)
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,42 +1,78 @@
-// Write your helper functions here!
+// Task 2: Adding Validation
+// validateInput() takes a string and returns "Empty", "Not a Number" or "Is a Number" if needed. The data from validateInput() will flow into formSubmission(). 
+function validateInput(testInput) 
+{if (testInput === '') {
+  return "Empty";
+} else if (isNaN(testInput)) {
+  return "Not a Number";
+} else {
+  return "Is a Number";
+    }
+}
+ //this function takes in several parameters and updates the UI based on the input values
+function formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass) {
+  let launchStatus = document.getElementById("launchStatus") 
+  let pilotStatus = document.getElementById("pilotStatus") 
+  let copilotStatus = document.getElementById("copilotStatus") 
+  let fuelStatus = document.getElementById("fuelStatus") 
+  let cargoStatus = document.getElementById("cargoStatus")
 
-require('cross-fetch/polyfill');
+    //check if pilotName, copilotName, fuelLevel, cargoMass are empty strings
+    if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
+      alert("All fields are required!");
+    }
+  
+    // makes sure fuelLevel and cargoMass are numbers
+    if (validateInput(fuelLevel) === "Not a Number" || validateInput(cargoMass) === "Not a Number") {
+      alert("fuelLevel and cargoMass must be numbers");
+    }
+      pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch`;
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
- }
- 
- function validateInput(testInput) {
-    
- }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
- 
+    //checks if cargoMass and fuelLevel are within appropriate ranges
+    if (cargoMass < 10000 && fuelLevel >= 10000) 
+    {
+      // list.style.visibility = "hidden";
+      launchStatus.innerHTML = "Shuttle is Ready for Launch";
+      launchStatus.style.color = "green";
+      cargoStatus.innerHTML = "Cargo mass low enough for launch";
+      fuelStatus.innerHTML = "Fuel level high enough for launch";
+    }
+  
+    // if fuel level is 10,000 or more, list stays hidden
+    if (fuelLevel >= 10000)
+    {
+      //  list.style.visibility = "hidden";
+      launchStatus.innerHTML = "Shuttle is Ready for Launch";
+      launchStatus.style.color = "green";
+      fuelStatus.innerHTML = "Fuel level high enough for launch";
+    } 
+    else 
+    // if fuelLevel is under 10,000, list becomes visible, "not ready" messages appear 
+    { 
+      list.style.visibility = "visible";
+      launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+      launchStatus.style.color = "red";
+      fuelStatus.innerHTML = "Fuel level too low for launch"; 
+    }
+    //check if cargo mass is <10,000
+    if (cargoMass < 10000) 
+    { 
+      // list.style.visibility = "hidden";
+      cargoStatus.innerHTML = "Cargo mass low enough for launch";
+    }
+    else 
+    //if cargoMass >= 10000, then show list and make red
+    { 
+      list.style.visibility = "visible";
+      launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+      launchStatus.style.color = "red";
+      cargoStatus.innerHTML = "Cargo mass too heavy for launch"; 
+    }    
+}  
+
+
+
  module.exports.addDestinationInfo = addDestinationInfo;
  module.exports.validateInput = validateInput;
  module.exports.formSubmission = formSubmission;
